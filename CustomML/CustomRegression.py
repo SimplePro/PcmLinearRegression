b7003e76b7d9fe1fc5f69ad8d01e329489729f5b
DonghwanKim <66504341+SimplePro@users.noreply.github.com>
2021-03-08 18:37:57 +0900

    added CustomRegression file

--- CustomML/CustomRegression.py
+++ CustomML/CustomRegression.py
@@ -4,91 +4,10 @@ import pandas as pd
 from functions_module import Functions
 
 
-# degree 가 1일 때 model 예측하는 클래스
-class PimDegree1:
-    def __init__(self, epoch=1000, dp=0.1):
-        if epoch is None:
-            raise Exception("epoch must not be None.")
+# PimLinearRegression Logic class
+class PimLinearRegressionLogic:
 
-        if dp is None:
-            raise Exception("dp must not be None.")
-
-        self.a = 0  # 기울기
-        self.b = 0  # y 절편
-        self.data = None  # X, y 데이터프레임
-        self.epoch = epoch  # 반복 횟수
-        self.dp = dp  # 데이터 전처리 단위
-
-    def fit(self, X=None, y=None):
-        if X is None or y is None:
-            raise Exception("x and y cannot be None.")
-
-        if not isinstance(X, np.ndarray) or not isinstance(y, np.ndarray):
-            raise Exception("X and y should be ndarray")
-
-        if len(X) != len(y):
-            raise Exception("X length and y length cannot be different")
-
-        self.fit_logic(X, y)
-
-    # 학습
-    def fit_logic(self, X, y):
-        up = []  # 데이터마다 증가량에 따른 기울기를 담는 리스트.
-
-        self.data = list(zip(X, y))
-        self.data = pd.DataFrame(self.data, columns=["X", "y"])
-
-        def duplicate(x):
-            duplicate_data = self.data[(self.data["X"] > (x - self.dp)) & (self.data["X"] < (x + self.dp))]["y"]
-            return sum(duplicate_data) / len(duplicate_data)
-
-        self.data["y"] = self.data["X"].apply(lambda x: duplicate(x))
-        self.data = self.data.drop_duplicates(["y"], keep="first")
-        self.data = self.data.reset_index(drop=True)
-
-        for i in range(self.epoch):
-            sample_xy = self.data.sample(n=2)
-            sample_xy = sorted([xy for xy in list(zip(sample_xy.iloc[:, 0].tolist(), sample_xy.iloc[:, 1].tolist()))],
-                               key=lambda x: x[0])
-            up.append((sample_xy[0][1] - sample_xy[1][1]) / (sample_xy[0][0] - sample_xy[1][0]))
-
-        # 기울기 구하기
-        self.a = round(sum(up) / len(up), 3)
-
-        # 절편 구하기
-        b_lst = self.data.iloc[:, 1] - (self.a * self.data.iloc[:, 0])
-        self.b = round(sum(b_lst) / len(b_lst), 3)
-
-    # 예측
-    def predict(self, X):
-        y_pred = []
-        for i in X:
-            y_pred.append(self.a * i + self.b)
-        return y_pred
-
-    # 정보
-    def info(self, ifpr=True):
-        if ifpr:
-            print(f"y = {self.a}x + ({self.b})")
-
-        return self.a, self.b
-
-    def evaluation_graph(self, X, y):
-        plt.scatter(X, y, label="original")
-        plt.scatter(self.data.iloc[:, 0], self.data.iloc[:, 1], color="red", label="preprocessing")
-
-        pred = self.predict(self.data.iloc[:, 0])
-        plt.plot(self.data.iloc[:, 0], pred, color="yellow", label="predict", linewidth=3.0)
-
-        plt.legend()
-
-        plt.show()
-
-
-# degree 가 2 이상일 때 model 예측하는 클래스
-class PimDegree2Up:
-
-    def __init__(self, epoch=10000, dp=0.1, degree=2):
+    def __init__(self, epoch=10000, dp=0.1, degree=1):
         if epoch is None:
             raise Exception("epoch must not be None.")
 
@@ -132,29 +51,26 @@ class PimDegree2Up:
         self.data = self.data.reset_index(drop=True)
 
         # degree 에 따라 유동적으로 구역 나누기
-        zone_unit = (self.data.iloc[-1, 0] - self.data.iloc[0, 0]) / (self.degree+1)  # 구역 단위        
+        zone_unit = (self.data.iloc[-1, 0] - self.data.iloc[0, 0]) / (self.degree+1)  # 구역 단위
         zones = [self.data[self.data["X"] < (self.data.iloc[0, 0] + zone_unit)]]
-        
-        for i in range(2, self.degree+1):
-            zones.append(self.data[(self.data["X"] >= (self.data.iloc[0, 0] + zone_unit * (i-1))) & (self.data["X"] < (self.data.iloc[0, 0] + zone_unit * i))])
-            
+
+        if self.degree >= 2:
+            for i in range(2, self.degree+1):
+                zones.append(self.data[(self.data["X"] >= (self.data.iloc[0, 0] + zone_unit * (i-1))) & (self.data["X"] < (self.data.iloc[0, 0] + zone_unit * i))])
+
         zones.append(self.data[self.data["X"] >= (self.data.iloc[0, 0] + zone_unit * self.degree)])
 
         for i in range(self.epoch):
-            try:
-                functions = Functions()
-                funcs = []
+            functions = Functions()
+            funcs = []
 
-                for idx in range(len(zones)):
-                    funcs.append(zones[idx].sample(n=1).iloc[0, :].tolist())
+            for idx in range(len(zones)):
+                funcs.append(zones[idx].sample(n=1).iloc[0, :].tolist())
 
-                for x, y in funcs:
-                    functions.add_func((x, y))
+            for x, y in funcs:
+                functions.add_func((x, y))
 
-                up.append(functions.predict_func())
-
-            except:
-                pass
+            up.append(functions.predict_func())
 
         for i in up:
             for j in range(len(i)):
@@ -208,14 +124,7 @@ class PimLinearRegression:
         if epoch is None:
             raise Exception("epoch must not be None.")
 
-        if degree == 1:
-            self.model = PimDegree1(epoch=epoch, dp=dp)
-
-        elif degree >= 2:
-            if epoch < 1000:
-                raise Exception("epoch must not be smaller than 1000")
-
-            self.model = PimDegree2Up(epoch=epoch, dp=dp, degree=degree)
+        self.model = PimLinearRegressionLogic(epoch=epoch, dp=dp, degree=degree)
 
     # 학습
     def fit(self, X=None, y=None):
